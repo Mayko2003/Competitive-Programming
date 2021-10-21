6af0041fe7b42f193b3c643b00ad6ca575e6d1c5
Mayko2003 <maykox34f@gmail.com>
2021-10-20 22:34:58 -0300

    init

---
+++ hoja1.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<fstream>
+#include<algorithm>
+#include<math.h>
+#include<vector>
+#define ff first
+#define ss second
+#define pb push_back
+#define pbk pop_back
+#define forn(i,n) for(int i = 0; i < n; i++)
+using namespace std;
+typedef long long ll;
+typedef vector<vector<int> > vvi;
+typedef vector<int> vi;
+typedef pair<int,int> pii;
+typedef pair<int, long long> pil;
+
+
+
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    ifstream cin("in.txt");
+    
+    
+    
+    cin.close();
+    return 0;
+}
