8bec571a03d8febf6a4df266a989d303c000b129
Mayko2003 <maykox34f@gmail.com>
2021-11-23 20:42:51 -0300

    update lca table

--- code/estructuras/lca_table.cpp
+++ code/estructuras/lca_table.cpp
@@ -1,50 +1,55 @@
-//aun en revision el calculo del lca
 #include<vector>
 #include<math.h>
-#define pb push_back
+#define forn(i,n) for(int i = 0; i < n; i++)
 using namespace std;
 typedef vector<int> vi;
 const int MAXN = 1e5 + 7,LOGN = 18, RAIZ = 0;
 
-vi travel, mat[MAXN], dist(MAXN);
+vi mat[MAXN], dist(MAXN);
 vector<bool> vis(MAXN,false);
-struct lca_table{
+struct lcatable{
     int sparse[MAXN][LOGN], level[MAXN];
 
-    void construct(int nodo, int lev){
-        level[nodo] = lev;
-        int t = travel.size();
-        for(int log = 0, p = 1; p <= t; log++, p *= 2) sparse[nodo][log] = travel[t - p];
+    //constructor
+    lcatable(){
+        forn(i,MAXN) forn(j,LOGN) sparse[i][j] = RAIZ;
+        level[RAIZ] = -1;
+    }
+
+    void initNode(int n, int p){
+        level[n] = level[p] + 1;
+        sparse[n][0] = p;
+        for(int i = 1; i < LOGN; i++) p = sparse[n][i - 1],sparse[n][i] = sparse[p][i - 1];
     }
 
     int lca(int u, int v){
-        if(level[u] < level[v]) swap(u,v);
-
-        int log = log2(level[u]);
-
-        //nivelar
-        for(int i = log; i >= 0; i--)
-            if(level[u] - (1<<i) >= level[v]) u = sparse[u][i];
-        
+        if(level[u] < level[v]) swap(u, v);
+        int i;
+        for(i = 0; level[u] - (1<<i) > level[v]; i++);
+        for(; level[u] > level[v]; i--){
+            if(level[u] - (1<<i) < level[v]) continue;
+            u = sparse[u][i];
+        }
         if(u == v) return u;
-
-        //subir
-        for(int i = log; i >= 0; i--)
-            if(sparse[u][i] != sparse[v][i]) u = sparse[u][i], v = sparse[v][i];
-        //retornar lca
+        for(i = 0; sparse[u][i] != sparse[v][i]; i++);
+        for(; i >= 0; i--){
+            if(sparse[u][i] == sparse[v][i]) continue;
+            u = sparse[u][i];
+            v = sparse[v][i];
+        }
         return sparse[u][0];
-
     }
 }lca;
 
-void dfsInit(int act, int padre, int lev){
-    vis[act] = true;
-    lca.construct(act,lev);
-    travel.pb(act);
-    for(auto hijo : mat[act]){
-        if(hijo == padre) continue;
-        dfsInit(hijo, act, lev + 1);
-        dist[hijo] = dist[act] + 1;
+void dfsInit(int n, int p){
+    vis[n] = true;
+    lca.initNode(n,p);
+    for(auto hijo : mat[n]){
+        if(hijo == p) continue;
+        dfsInit(hijo, n);
+        dist[hijo] = dist[n] + 1;
     }
-    travel.pop_back();
+}
+int main(){
+    return 0;
 }
